4a46911a9e2189527ef532d962e5f1e8615f38a6
codewithsuraj <mail2surajmahendrakar@gmail.com>
2024-10-09 11:04:32 +0530

    29.11 Handling unexpected errors

--- app/users/[id]/not-found.tsx
+++ app/users/[id]/not-found.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const UserNotFoundPage = () => {
-  return <div>This user doesn't exists.</div>;
+  return <div>This user does not exists.</div>;
 };
 
 export default UserNotFoundPage;
