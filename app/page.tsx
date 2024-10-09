fda29ceb252fde6e9fc3ad45b7c59f5357a6bb77
codewithsuraj <mail2surajmahendrakar@gmail.com>
2024-10-09 22:56:10 +0530

    73.2 Optimizing images

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
+
+import suraj from "@/public/images/suraj.jpg";
+
 export default async function Home() {
-  const session = await getServerSession(authOptions);
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user!.name} </span>}</h1>
-      <Link href={"/users"}>Users</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      {/* <Image src={suraj} alt="Srk" /> */}
+      {/* <Image
+        src="https://bit.ly/react-cover"
+        alt="Srk"
+        width={300}
+        height={170}
+      /> */}
+
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="Srk"
+        fill
+        className="object-cover"
+        // sizes="100vw"
+        sizes="(max-width: 480px) : 100vw, (max-width: 768px) : 50vw, 33vw"
+        quality={75}
+        priority
+      />
     </main>
   );
 }
